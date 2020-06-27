966d9810168e1e1f5297b2f05a7b5d16be9825f0
Pedro Sanchez <pedro.sanchez@shopify.com>
2020-06-27 00:14:03 -0400

    Use the Path lib to search for the diagram files

--- pdiagrams/cli.py
+++ pdiagrams/cli.py
@@ -1,43 +1,10 @@
 import os
 import argparse
 from pathlib import Path
-from typing import List
 
 from pdiagrams import __version__ as version
 
 VERSION = f'Print Diagrams v{version}'
-
-
-def find_diagrams_files(paths: List) -> List:
-    diags = []
-    for p in paths:
-        diags += [x for x in p.iterdir() if x.is_file() and x.suffixes == ['.diag', '.py']]
-    return diags
-
-
-def find_directories(root: str) -> List:
-    def dir_walk(p: List) -> List:
-        if not p:
-            return p
-        dirs = []
-        for d in p:
-            dirs += [x for x in d.iterdir() if x.is_dir()]
-        return p + dir_walk(dirs)
-
-    path = Path(root)
-    if not path.is_dir():
-        raise FileNotFoundError(f'no such directory "{root}"')
-    return dir_walk([path])
-
-
-def print_diagrams(diags: List):
-    working_dir = Path.cwd()
-    for d in diags:
-        fig = d.read_text()
-        os.chdir(d.parent)
-        print(d)
-        exec(fig)
-        os.chdir(working_dir)
 
 
 def parse_arguments():
@@ -53,17 +20,28 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def print_diagrams(diagrams):
+    working_dir = Path.cwd()
+    for d in diagrams:
+        fig = d.read_text()
+        os.chdir(d.parent)
+        print(d)
+        exec(fig)
+        os.chdir(working_dir)
+
+
+def find_diagrams(roots):
+    print(VERSION)
+    diagrams = []
+    for root in roots:
+        p = Path(root)
+        diagrams += list(p.glob('**/*.diag.py'))
+    print_diagrams(diagrams)
+
+
 def main():
     args = parse_arguments()
-    print(VERSION)
-    for root in args.root:
-        try:
-            dirs = find_directories(root)
-        except FileNotFoundError as err:
-            print(f'FileNotFoundError: {err}')
-        else:
-            diags = find_diagrams_files(dirs)
-            print_diagrams(diags)
+    find_diagrams(args.root)
 
 
 if __name__ == "__main__":
